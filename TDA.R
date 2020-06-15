d60afbb687d877b4f99743f7d507bd5f07f49886
Jake Ireland <os19582@bristol.ac.uk>
2020-06-15 20:59:05 +0100

    Colour now based on low/medium/high yield

--- TDA.R
+++ TDA.R
@@ -43,10 +43,29 @@ library(networkD3)
 nodes <- mapperVertices(mapper, 1:dim(X)[1])
 links <- mapperEdges(mapper)
 
-forceNetwork(Nodes = nodes, Links = links,
-             Source = "Linksource", Target="Linktarget",
-             Value = "Linkvalue", NodeID="Nodename",
-             Group = "Nodegroup", opacity=0.8,
-             zoom = TRUE, Nodesize="Nodesize")
+node.yields <- c()
+for (i in 1:length(nodes$Nodename)) {
+  n <- nodes$Nodename[i]
+  x <- strsplit(toString(n),":")[[1]][2]
+  indicies <- as.numeric(strsplit(x,split=", ",fixed=TRUE)[[1]])
+  node.yields <- c(node.yields, mean(train.data[indicies,]$Yield))
+}
 
+range <- max(node.yields) - min(node.yields)
+min = as.numeric(min(node.yields))
 
+node.yields[node.yields <= min + (1/3)*range] = "Low"
+node.yields[min + (1/3)*range < node.yields & node.yields <= min + (2/3)*range] = "Medium"
+node.yields[node.yields != "Low" & node.yields != "Medium"] = "High"
+
+nodes$Nodegroup <- node.yields
+
+network <- forceNetwork(Nodes = nodes, Links = links,
+                        Source = "Linksource", Target="Linktarget",
+                        Value = "Linkvalue", NodeID="Nodename",
+                        Group = "Nodegroup", opacity=0.8,
+                        zoom = TRUE, Nodesize="Nodesize")
+
+library(magrittr)
+
+saveNetwork(network, file = 'Net1.html')
